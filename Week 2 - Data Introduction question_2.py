b20a4c8cab90f628e9c359939ad26f0e8e9b3a5f
Sangeetha1110 <71755815+Sangeetha1110@users.noreply.github.com>
2020-09-27 15:52:04 +0530

    Add files via upload

---
+++ Week 2 - Data Introduction question_2.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Understanding:
+
+# Dataset contains several attributes such as:
+# 
+# 1.SEVERITYCODE
+# 
+# 2.X
+# 
+# 3.Y
+# 
+# 4.OBJECTID
+# 
+# 5.INCKEY
+# 
+# 6.COLDETKEY
+# 
+# 7.REPORTNO
+# 
+# 8.STATUS
+# 
+# 9.ADDRTYPE
+# 
+# 10.INTKEY
+# 
+# 11.LOCATION
+# 
+# 12.EXCEPTRSNCODE
+# 
+# 13.EXCEPTRSNDESC
+# 
+# 14.SEVERITYCODE.1
+# 
+# 15.SEVERITYDESC
+# 
+# 16.COLLISIONTYPE
+# 
+# 17.PERSONCOUNT
+# 
+# 18.PEDCOUNT
+# 
+# 19.PEDCYLCOUNT
+# 
+# 20.VEHCOUNT
+# 
+# 21.INCDATE
+# 
+# 22.INCDTTM
+# 
+# 23.JUNCTIONTYPE
+# 
+# 24.SDOT_COLCODE
+# 
+# 25.SDOR_COLDESC
+# 
+# 26.INATTENTIONIND
+# 
+# 27.UNDERINFL
+# 
+# 28.WEATHER
+# 
+# 29.ROADCOND
+# 
+# 30.LIGHTCOND
+# 
+# 31.PEDROWNOTGRNT
+# 
+# 32.SDOTCOLUMN
+# 
+# 33.SPEEDING
+# 
+# 34.ST_COLCODE
+# 
+# 35.ST_COLDESC
+# 
+# 36.SEGLANEKEY
+# 
+# 37.CROSSWALKKEY
+# 
+# 38.HITPARKEDCAR
+
+# # Data Preparation:
+
+# The target variable we will be considering for the analysis will be - 'SEVERITYCODE' as it used to depict the severity of the accident. The same is denoted as 0 or 1 within the dataset; where
+# 
+# "0" denotes Property damage
+# "1" denotes Severe Injury
+
+# # Attributes :
+
+# We analyzed that the attributes used to describe the severity of an accident are:
+# 
+# "WEATHER"
+# "ROADCOND"
+# "ADDRTYPE"
+# "COLLISIONTYPE"
+# "LIGHTCOND"
+
+# # Data Cleaning
+
+# The data needs to be cleanned as it is not fit for analysis. There are unncessary columns and also the datatypes needs changing.
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
